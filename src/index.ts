406d92bb78be854135c2a3c0e267768819f5cec5
RomelSikdar <romels500@gmail.com.com>
2023-09-27 21:56:30 +0530

    removal of not needed files

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { EcoFlow } from "@eco-flow/core";
-
-new EcoFlow().start();
-
-console.log("====================================");
-console.log(ecoFlow);
-console.log("====================================");
