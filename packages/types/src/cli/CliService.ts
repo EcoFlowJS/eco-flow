f55183b5923cfeb7c0930fba56f8575d9d620221
RomelSikdar <romels500@gmail.com.com>
2024-02-06 08:31:38 +0530

    Types and interfaces updated

---
+++ packages/types/src/cli/CliService.ts
@@ -0,0 +1,9 @@
+import { ICommand } from "./commands";
+
+export interface CliService {
+  get serviceStatus(): "Stopped" | "Running" | "Restarting";
+
+  startService(args: ICommand): void;
+  stopService(): void;
+  restartService(args: ICommand): void;
+}
--- packages/types/src/cli/CommanderCli.ts
+++ packages/types/src/cli/CommanderCli.ts
@@ -1,7 +1,10 @@
+import { CliService } from "./CliService";
+
 export interface CommanderCli {
   parseArgs(): CommanderCli;
   usesMsgs(str: string): CommanderCli;
   get args(): {
     [key: string]: any;
   };
+  get CliService(): CliService;
 }
