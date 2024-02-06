31d1f976be77ed339b04ee92ee4c9a991e8561bb
RomelSikdar <romels500@gmail.com.com>
2024-02-06 08:39:52 +0530

    CliService interface fix

--- packages/types/src/cli/CliService.ts
+++ packages/types/src/cli/CliService.ts
@@ -3,7 +3,7 @@ import { ICommand } from "./commands";
 export interface CliService {
   get serviceStatus(): "Stopped" | "Running" | "Restarting";
 
-  startService(args: ICommand): void;
+  startService(args?: ICommand): void;
   stopService(): void;
   restartService(args: ICommand): void;
 }
