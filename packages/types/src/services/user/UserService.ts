83f3ee24937eaf7184390f78e7e1012c4cfa42c1
RomelSikdar <romels500@gmail.com.com>
2024-02-16 13:02:51 +0530

    userService type updated

--- packages/types/src/services/user/UserService.ts
+++ packages/types/src/services/user/UserService.ts
@@ -1,3 +1,21 @@
+import { ApiResponse } from "../../ecoflow";
+
 export interface UserService {
   isNoUser(): Promise<boolean>;
+  createUser(
+    userInfo: userTableCollection,
+    isAdmin?: boolean
+  ): Promise<ApiResponse>;
+}
+
+export interface userTableCollection {
+  _id?: string;
+  name: string;
+  username: string;
+  password: string;
+  email?: string;
+  oldPassword?: string;
+  isPermanent?: boolean;
+  created_at?: Date;
+  updated_at?: Date;
 }
