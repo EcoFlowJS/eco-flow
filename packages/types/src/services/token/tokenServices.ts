97209e4c8dcb47dd07215b8da996842cb0955aad
RomelSikdar <romels500@gmail.com.com>
2024-02-17 22:28:24 +0530

    token type updated

--- packages/types/src/services/token/tokenServices.ts
+++ packages/types/src/services/token/tokenServices.ts
@@ -1,4 +1,5 @@
 export interface TokenServices {
   checkToken(token: string, userId: string): Promise<boolean>;
   generateToken(_id: string): Promise<[string, string, number]>;
+  removeToken(token: string, userId: string): Promise<void>;
 }
