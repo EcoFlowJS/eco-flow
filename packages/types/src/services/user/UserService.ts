2e10990877ccf0819175c1501520f57f83aa0a11
RomelSikdar <romels500@gmail.com.com>
2024-02-17 19:55:14 +0530

    types updates

--- packages/types/src/services/user/UserService.ts
+++ packages/types/src/services/user/UserService.ts
@@ -6,16 +6,26 @@ export interface UserService {
     userInfo: userTableCollection,
     isAdmin?: boolean
   ): Promise<ApiResponse>;
+  getUserAllInfo(username: string): Promise<GetUserInfo>;
 }
 
-export interface userTableCollection {
+interface UserInfo {
   _id?: string;
   name: string;
   username: string;
-  password: string;
+  isActive: boolean;
   email?: string;
   oldPassword?: string;
   isPermanent?: boolean;
   created_at?: Date;
   updated_at?: Date;
+}
+
+export interface GetUserInfo {
+  isAvailable: boolean;
+  user?: userTableCollection;
+}
+
+export interface userTableCollection extends UserInfo {
+  password: string;
 }
