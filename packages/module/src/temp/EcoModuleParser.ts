61cbf6d6c2e94ce70ef39e1f8cac416ab71b3b9b
RomelSikdar <romels500@gmail.com.com>
2024-03-30 15:03:45 +0530

    Module Removed Rebuild in Process

---
+++ packages/module/src/temp/EcoModuleParser.ts
@@ -0,0 +1,24 @@
+import { EcoModuleParser as IEcoModuleParser } from "@ecoflow/types";
+import fse from "fs-extra";
+import _ from "lodash";
+
+export class EcoModuleParser implements IEcoModuleParser {
+  constructor() {}
+
+  /**
+   * Extract the controller function from the template file and return the controller function.
+   * @param controllerPath Location of the controller file that contains the working sinippets of the controller.
+   * @returns {Function} Controller function that can be used in the middleware.
+   */
+  static processControllers(controllerPath: string): Function {
+    let controller: Function = (ctx: any) => ctx;
+    if (!fse.existsSync(controllerPath)) return controller;
+    try {
+      const tempController = require(controllerPath);
+      if (!_.isFunction(tempController)) return controller;
+      return tempController;
+    } catch {
+      return controller;
+    }
+  }
+}
