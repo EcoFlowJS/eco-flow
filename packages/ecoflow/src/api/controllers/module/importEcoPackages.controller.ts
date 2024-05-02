7dd8da739f9db93e92191974a218e172b8e662df
RomelSikdar <romels500@gmail.com.com>
2024-05-02 23:49:32 +0530

    Local package implementation added

--- packages/ecoflow/src/api/controllers/module/importEcoPackages.controller.ts
+++ packages/ecoflow/src/api/controllers/module/importEcoPackages.controller.ts
@@ -1,21 +1,42 @@
-import { ModuleSchema } from "@ecoflow/types";
+import { ApiResponse, ModuleSchema } from "@ecoflow/types";
 import { Upload } from "@ecoflow/utils";
 import { Context } from "koa";
 
 const importEcoPackages = async (ctx: Context) => {
-  const { ecoModule } = ecoFlow;
-  if (ctx.request.files && ctx.request.files["packages"]) {
-    const uploadedFiles = await new Upload(
-      ctx.request.files["packages"]
-    ).upload();
+  const { _, ecoModule, service } = ecoFlow;
+
+  try {
+    if (
+      _.isUndefined(ctx.request.files) ||
+      _.isUndefined(ctx.request.files["packages"]) ||
+      _.isEmpty(ctx.request.files["packages"])
+    )
+      throw "Packages is required.";
 
     const schemas: ModuleSchema[] = await ecoModule.installLocalModule(
-      uploadedFiles
+      await new Upload(ctx.request.files["packages"]).upload()
     );
-
     ecoModule.addModule(schemas);
+
+    ctx.status = 200;
+    ctx.body = <ApiResponse>{
+      success: true,
+      payload: schemas.map((s) => s.module),
+    };
+    for await (const { module: installedModule } of schemas) {
+      await service.AuditLogsService.addLog({
+        message: `New local package(${installedModule?.name}) has been installed by ${ctx.user}`,
+        type: "Info",
+        userID: ctx.user,
+      });
+    }
+  } catch (error) {
+    ctx.status = 404;
+    ctx.body = <ApiResponse>{
+      error: true,
+      payload: error,
+    };
   }
-  ctx.body = {};
 };
 
 export default importEcoPackages;
