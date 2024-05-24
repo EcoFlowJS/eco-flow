26883b15c769e936e0a4541d2a2d3942cb955154
RomelSikdar <romels500@gmail.com.com>
2024-05-25 01:46:29 +0530

    minor fixes

--- run.ts
+++ run.ts
@@ -1,3 +1,3 @@
 import { CommanderCli } from "@ecoflow/cli";
-new CommanderCli().CliService.startService({ auth: true });
+new CommanderCli().CliService.startService({ auth: true, dev: true });
 // new CommanderCli().CliService.startService();
