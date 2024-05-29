5562dcf903d5aa03cc6be404e6bdb50c6e6d755a
RomelSikdar <romels500@gmail.com.com>
2024-05-29 10:49:45 +0530

    backup restore from server saved file implemented

---
+++ packages/ecoflow/src/api/controllers/backupRestore/backedFilesFetch.controller.ts
@@ -0,0 +1,21 @@
+import { ApiResponse } from "@ecoflow/types";
+import { Context } from "koa";
+import fetchBackupFiles from "../../helpers/fetchBackupFiles";
+
+const backedFilesFetch = async (ctx: Context) => {
+  try {
+    ctx.status = 200;
+    ctx.body = <ApiResponse>{
+      success: true,
+      payload: await fetchBackupFiles(),
+    };
+  } catch (error) {
+    ctx.status = 409;
+    ctx.body = <ApiResponse>{
+      error: true,
+      payload: error,
+    };
+  }
+};
+
+export default backedFilesFetch;
--- packages/ecoflow/src/api/routes/backupRestore/backupRestore.routes.ts
+++ packages/ecoflow/src/api/routes/backupRestore/backupRestore.routes.ts
@@ -1,6 +1,8 @@
 import { EcoRouter } from "../../../service/EcoRouter";
+import backedFilesFetch from "../../controllers/backupRestore/backedFilesFetch.controller";
 import backupRestoreStatus from "../../controllers/backupRestore/backupRestoreStatus.controller";
 import generateBackupFile from "../../controllers/backupRestore/generateBackupFile.controller";
+import removeBackedFile from "../../controllers/backupRestore/removeBackedFile.controller";
 import restoreBackup from "../../controllers/backupRestore/restoreBackup.controller";
 import isAuthenticated from "../../controllers/user/isAuthenticated.controller";
 
@@ -8,5 +10,13 @@ const backupRestoreRouter = EcoRouter.createRouter();
 export default backupRestoreRouter;
 
 backupRestoreRouter.get("/", isAuthenticated, backupRestoreStatus);
+
+backupRestoreRouter.get("/backup", isAuthenticated, backedFilesFetch);
 backupRestoreRouter.post("/backup", isAuthenticated, generateBackupFile);
+backupRestoreRouter.delete(
+  "/backup/:fileName",
+  isAuthenticated,
+  removeBackedFile
+);
+
 backupRestoreRouter.post("/restore", isAuthenticated, restoreBackup);
